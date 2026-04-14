789aaa32db5be127301187da7810082a0866aedd
Shuaib <shuaib@seamlesshr.com>
2026-04-14 09:48:07 +0100

    Correct database name casing in connection config

--- vuln.js
+++ vuln.js
@@ -7,7 +7,7 @@ const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'password',
-  database: 'testdb'
+  database: 'testDB'
 });
 
 app.get('/user', (req, res) => {
